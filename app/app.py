1178448c2262473f5443904a1c329a119cf2f7fc
WeustiS <willceustis@gmail.com>
2020-04-26 20:55:37 -0400

    loading model

--- app/app.py
+++ app/app.py
@@ -1,16 +1,13 @@
 from flask import Flask, render_template, request
+import tensorflow as tf
+# YOU NEED TF 2.1 and PROTOBUF 3.11!!!
+import os
 
 app = Flask('image_optimizer')
 
-from types import SimpleNamespace
-
-
-def dummy():
-    return 123
-
 
 def get_model():
-    model = SimpleNamespace(predict=dummy)
+    model = tf.keras.models.load_model('model42604.h5')
     return model
 
 
@@ -35,10 +32,11 @@ def results():
     if request.method == 'POST':
         # write your function that loads the model
         model = get_model()  # you can use pickle to load the trained model
+
         # year = request.form['year']
         # pred = model.predict()
-        image_scores = run_model(model, request.files)
-        return render_template('result.html', pred=image_scores)
+        # image_scores = run_model(model, request.files)
+        return render_template('result.html', pred=123)
 
 
 app.run("localhost", 9999, debug=True)
