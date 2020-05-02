86568dfa68ad4989a0901d0fae5ce2304002960c
ishitarao <ir5@illinois.edu>
2020-05-02 17:25:31 -0500

    Add logic to display multiple images on the html page (untested)

--- app/app.py
+++ app/app.py
@@ -1,16 +1,39 @@
-import os
+import io
 import numpy as np
 import cv2
-from flask import Flask, render_template, request
+from PIL import Image
 import tensorflow as tf
+import base64
+import os
+from flask import Flask, render_template, request
+import optimizer
+
 # YOU NEED TF 2.1.0, CV2 4.2.0.34, and PROTOBUF 3.11.3!!!
-# You also need pillow
+# You also need pillow and opencv-python
+from image_augment import ImageAugment
+
 app = Flask('image_optimizer')
 
 
 def get_model():
-    model = tf.keras.models.load_model('model_ver2.h5')
+    model = tf.keras.models.load_model('/Users/ishita/team7/app/model_ver2.h5')
     return model
+
+
+# THIS FUNCTION USES PLACEHOLDERS: don't expect this to work right now
+def run_model(model, files):
+    for image in files:
+        augmenter = ImageAugment(image)
+        augmenter.augment()
+
+    augmented_images = model.augment(files)
+    scores = model.calculate_socres(augmented_images)  # list or a dict
+    best_images = {}
+    for index in range(scores.len):
+        best_images.update({augmented_images.get(index): scores[index]})
+
+    return best_images
+
 
 def get_images(files):
     """converts request.files to an array of PIL images"""
@@ -21,6 +44,7 @@ def get_images(files):
         image = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
         arr.append(image)
         return arr
+
 
 def create_tensor(images):
     """converts an array of PIL images to a single
@@ -43,18 +67,27 @@ def create_tensor(images):
     crop_size = [200, 200]
     return tf.image.crop_and_resize(tensor, boxes, boxes_ind, crop_size)
 
-def save_images():
+
+def save_images(images):
     """ saves PIL images as .jpg's to be used by result.html """
-    return
+    image_objects = []
+    for image in images:
+        # convert numpy array to PIL Image
+        img = Image.fromarray(image)
 
-def run_model(model, files):
-    augmented_images = model.augment(files)
-    scores = model.calculate_socres(augmented_images)  # list or a dict
-    image_scores = {}
-    for index in range(scores.len):
-        image_scores.update({augmented_images.get(index): scores[index]})
+        # create file-object in memory
+        img_buffer = io.BytesIO()
 
-    return image_scores
+        # write JPEG in file-object
+        img.save(img_buffer, 'JPEG')
+
+        # move to beginning of file
+        img_buffer.seek(0)
+        img_tag = 'data:image/jpeg;base64,' + base64.b64encode(img_buffer.getvalue()).decode('ascii')
+        image_objects.append(img_tag)
+
+    return image_objects
+
 
 @app.route('/')
 def show_index():
@@ -66,14 +99,11 @@ def results():
     print(request.files.getlist('img'))
     if request.method == 'POST':
         # write your function that loads the model
-        model = get_model() # you can use pickle to load the trained model
-        images = get_images(request.files)
-        tensor = create_tensor(images)
+        images = optimizer.run(request.files)
         # year = request.form['year']
         # pred = model.predict()
-        # image_scores = run_model(model, request.files)
         
-        return render_template('result.html', images=images) # returns nothing useful for now
+        return render_template('result.html', images=images)  # returns nothing useful for now
 
 
 app.run("localhost", 9999, debug=True)
--- optimizer.py
+++ optimizer.py
@@ -1,13 +1,20 @@
+import io
+import numpy as np
+import cv2
 from PIL import Image
 import tensorflow as tf
+import base64
 
 
 # calls the other functions to return the dict containing the best images
+from image_augment import ImageAugment
+
+
 def run(images):
-    processed_images = run_preprocessor(images)
-    model = get_model()
-    best_images = run_model(model, processed_images)
-    return best_images
+    pil_images = get_images(images)
+    tensor = create_tensor(pil_images)
+    # best_images = run_model(get_model(), tensor)
+    return save_images(tensor)
 
 
 def get_model():
@@ -15,8 +22,12 @@ def get_model():
     return model
 
 
-# returns a dict containing the image and its score
+# THIS FUNCTION USES PLACEHOLDERS: don't expect this to work right now
 def run_model(model, files):
+    for image in files:
+        augmenter = ImageAugment(image)
+        augmenter.augment()
+
     augmented_images = model.augment(files)
     scores = model.calculate_socres(augmented_images)  # list or a dict
     best_images = {}
@@ -26,13 +37,55 @@ def run_model(model, files):
     return best_images
 
 
-# resizes images and does whatever else a preprocessor is supposed to do
-def run_preprocessor(images):
-    processed_images = []
+def get_images(files):
+    """converts request.files to an array of PIL images"""
+    arr = []
+    for key, value in files.items():
+        filestr = value.read()
+        npimg = np.fromstring(filestr, np.uint8)
+        image = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+        arr.append(image)
+        return arr
 
-    for img_path in images:
-        image = Image.open(img_path)
-        image = image.resize((200, 200))
-        processed_images.append(image)
 
-    return processed_images
+def create_tensor(images):
+    """converts an array of PIL images to a single
+    4D tensor that is ready to be handled directly by the model"""
+    tensor = []
+    boxes = []
+    for image in images:
+        img_tensor = tf.keras.preprocessing.image.img_to_array(image)
+        tensor.append(img_tensor)
+        center_x = len(img_tensor[0])/2
+        center_y = len(img_tensor)/2
+        distance = min(center_x, center_y)
+        y1 = center_y - distance
+        x1 = center_x - distance
+        y2 = center_y + distance
+        x2 = center_x + distance
+        row = [y1, x1, y2, x2]
+        boxes.append(row)
+    boxes_ind = range(len(tensor))
+    crop_size = [200, 200]
+    return tf.image.crop_and_resize(tensor, boxes, boxes_ind, crop_size)
+
+
+def save_images(images):
+    """ saves PIL images as .jpg's to be used by result.html """
+    image_objects = []
+    for image in images:
+        # convert numpy array to PIL Image
+        img = Image.fromarray(image)
+
+        # create file-object in memory
+        img_buffer = io.BytesIO()
+
+        # write JPEG in file-object
+        img.save(img_buffer, 'JPEG')
+
+        # move to beginning of file
+        img_buffer.seek(0)
+        img_tag = 'data:image/jpeg;base64,' + base64.b64encode(img_buffer.getvalue()).decode('ascii')
+        image_objects.append(img_tag)
+
+    return image_objects
